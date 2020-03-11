f142e9ed9843e683317151c99c63e0064133a0e8
Jortega23 <jortegs23@gmail.com>
2020-03-10 22:51:55 -0700

    WIP: Adding to React
    Added react router to switch between components

--- recipe-app/src/components/Recipes.js
+++ recipe-app/src/components/Recipes.js
@@ -6,13 +6,20 @@ const Recipes = props => (
       {props.recipes.map((recipe)=>{
           return (
             <div className='col-md-4'>
-              <div className='recipe__box'>
+              <div className='recipes__box'>
                 <img className='recipe__box-img' 
                   src={recipe.recipe.image} 
                   alt={recipe.recipe.label}/>
                 <div className='recipe__text'>
-                  <h5>{recipe.recipe.label}</h5>
+                  <h5 className='recipes__title'>
+                    {recipe.recipe.label.length < 20 ? `${recipe.recipe.label}` : `${recipe.recipe.label.substring(0, 25)}...`}
+                  </h5>
+                  <p className='recipes__subtitle'>Publisher: <span>
+                    {recipe.recipe.source}
+                    </span>
+                  </p> 
                 </div>
+                  <button className='recipe_buttons'>View Recipe</button>
               </div> 
             </div>
           );
