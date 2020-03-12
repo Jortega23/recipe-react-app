e9afd4c4160cb7c6522200f0c8e52a478e424339
Jortega23 <jortegs23@gmail.com>
2020-03-11 22:33:12 -0700

    Wip adding route
    added routes

--- recipe-app/src/components/Recipes.js
+++ recipe-app/src/components/Recipes.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 const Recipes = props => (
     <div className='container'>
       <div className='row'>
@@ -19,7 +21,9 @@ const Recipes = props => (
                     </span>
                   </p> 
                 </div>
-                  <button className='recipe_buttons'>View Recipe</button>
+                  <button className='recipe_buttons'>
+                    <Link to={{ pathname: `/recipe/${recipe.recipe_id}`}}>View Recipe</Link>
+                  </button>
               </div> 
             </div>
           );
--- recipe-app/src/components/Router.js
+++ recipe-app/src/components/Router.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import App from '../App'
+import Recipe from './Recipe'
 
 const Router = () =>(
     <BrowserRouter>
-        <Route path='/' component={App} />
+    <Switch>
+        <Route exact path='/' component={App} />
+        <Route path='/recipe/:id' component={Recipe}/> 
+    </Switch>
     </BrowserRouter>
 );
+
+export default Router;
